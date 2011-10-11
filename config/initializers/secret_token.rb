c14d2d66913619fe54f70e7fb07d65c241068249
Nick Zadrozny <nick@beyondthepath.com>
2011-10-11 12:09:49 -0700

    Sample app for Solr advanced features with Sunspot and Rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Productastic::Application.config.secret_token = '5aa78d846750f32bb4bbbad7a5c377f9b30b9251acba24ea2cf235617a2ec8ff22fa34e516ecb7e45ba6d78eba1dbe935c57481fffb772a1f695199510e73f6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Productastic::Application.config.session_store :cookie_store, key: '_productastic_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Productastic::Application.config.session_store :active_record_store

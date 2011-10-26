fd5c5f615bf836e6fcb75c301a8cdb8ab09ae8df
Nick Zadrozny <nick@beyondthepath.com>
2011-10-25 19:47:54 -0700

    Prettifying the schema, adding a text_en type for stemming.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   
   searchable do
     text :name, stored: true, default_boost: 10
+    text :name, as: 'name_text_en'
     text :description, stored: true
   end
   
